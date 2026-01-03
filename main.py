6221fb53577c47e318a832252b3b9303aeb86f91
Nathan <nathan.sujatno@gmail.com>
2026-01-02 23:25:13 -0600

    added tracking for the top or bottom bar

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import numpy as np
 import pyautogui
 import time
 import win32gui
+import os
 
 title = "Stardew Valley"
 hwnd = win32gui.FindWindow(None, title)
 left, top, right, bottom = win32gui.GetWindowRect(hwnd)  # window coords[web:94]
+
+print(left, right, top, bottom)
 
 win_w = right - left
 win_h = bottom - top
@@ -19,13 +22,23 @@ WATCH_BOX = {
   "width": BOX_SIZE,
   "height": BOX_SIZE,
 }
+
+BAR_BOX = {
+  "left": left + int(win_w / 5),
+  "top": top,
+  "width": win_w - (int(win_w / 5) * 2),
+  "height": win_h
+}
 print(WATCH_BOX)
 
-CONFIDENCE = 0.75
+CONFIDENCE = 0.7
+BAR_CONFIDENCE = 0.9
 cast = True
 
 sct = mss.mss()
 exclamation = cv2.imread('images/exclamation.png', cv2.IMREAD_GRAYSCALE)
+top_of_bar = cv2.imread('images/top_of_bar.png', cv2.IMREAD_GRAYSCALE)
+bottom_of_bar = cv2.imread('images/bottom_of_bar.png', cv2.IMREAD_GRAYSCALE)
 
 def cast_rod():
   print("Casting rod...")
@@ -47,16 +60,60 @@ def watch_for_bite():
       return True
 
     time.sleep(0.01)
+  
+def watch_for_bar():
+  while True:
+    img = np.array(sct.grab(BAR_BOX))
+    frame = img[:, :, :3]
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    res = cv2.matchTemplate(gray, top_of_bar, cv2.TM_CCOEFF_NORMED)
+    res2 = cv2.matchTemplate(gray, bottom_of_bar, cv2.TM_CCOEFF_NORMED)
+    _, max_val, _, max_loc = cv2.minMaxLoc(res)
+    _, max_val_2, _, max_loc_2 = cv2.minMaxLoc(res2)
+
+    if max_val >= BAR_CONFIDENCE or max_val_2 >= BAR_CONFIDENCE:
+      print(f"Found the top bar at: {max_loc}")
+      print(f"Found the bottom bar at: {max_loc_2}")
+      # return True
+    
+    time.sleep(0.01)
+
+# helper function to determine boxes
+def get_rel_coords():
+  while True:
+      # Get current absolute mouse position
+      mouse_x, mouse_y = pyautogui.position()
+      
+      # Calculate relative position inside the window
+      rel_x = mouse_x - left
+      rel_y = mouse_y - top
+      
+      # Clear console for clean reading (Windows)
+      os.system('cls') 
+      
+      print(f"Window Location: ({left}, {top})")
+      print(f"Mouse Absolute:  ({mouse_x}, {mouse_y})")
+      print("-" * 30)
+      print(f"REL OFFSET X: {rel_x}") 
+      print(f"REL OFFSET Y: {rel_y}")
+      
+      time.sleep(0.1)
 
 print("starting in 5 seconds")
 time.sleep(5)
 
 while cast:
-  cast_rod()
+  # cast_rod()
 
-  if watch_for_bite():
-    print("hook fish")
-    pyautogui.mouseDown()
-    time.sleep(0.1)
-    pyautogui.mouseUp()
+  # if watch_for_bite():
+  #   print("hook fish")
+  #   pyautogui.mouseDown()
+  #   time.sleep(0.1)
+  #   pyautogui.mouseUp()
+  #   cast = False
+
+  print("watching for bar")
+  if watch_for_bar():
+    print("bar found")
     cast = False
